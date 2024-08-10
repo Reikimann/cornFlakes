d5f0b89cacb54048bce4bf83b9fc0d4cfc961df2
Reikimann <cbirkmand@gmail.com>
2024-08-10 18:26:59 +0200

    Configured yazi

--- home/modules/cli/yazi/default.nix
+++ home/modules/cli/yazi/default.nix
@@ -8,11 +8,13 @@
         show_hidden = false;
         ratio = [ 2 3 5 ];
         linemode = "size";
+        scrolloff = 15;
       };
     };
   };
 
   home.file.".config/yazi/theme.toml".source = ./theme.toml;
+  home.file.".config/yazi/keymap.toml".source = ./keymap.toml;
   home.file.".config/yazi/plugins".source = ./plugins;
   home.file.".config/yazi/init.lua".source = ./init.lua;
 }
