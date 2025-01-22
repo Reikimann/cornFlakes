f02368aa5686db42dc740803435db3374c929fdf
Reikimann <cbirkmand@gmail.com>
2025-01-22 10:25:12 +0100

    Added kitty module

--- home.nix
+++ home.nix
@@ -16,6 +16,7 @@
         disableWayland = true;
       };
       discord.enable = true;
+      kitty.enable = true;
     };
   };
 
--- home/modules/programs/kitty.nix
+++ home/modules/programs/kitty.nix
@@ -1,25 +1,46 @@
-{ ... }:
+{ config, lib, ... }:
 
+with lib;
+let
+  cfg = config.reiki.modules.programs.kitty;
+in
 {
-  home.sessionVariables = {
-    TERMINAL = "kitty";
+  options.reiki.modules.programs.kitty = {
+    enable = mkEnableOption "Kitty terminal configuration";
+    font = mkOption {
+      description = "Override font";
+      default = "Liga SFMono Nerd Font";
+      type = types.str;
+    };
+    fontSize = mkOption {
+      description = "Override font size";
+      default = 14;
+      type = types.int;
+    };
   };
 
-  programs.kitty = {
-    enable = true;
-    font = {
-      name = "Liga SFMono Nerd Font";
-      size = 14.0;
+  config = mkIf cfg.enable {
+    programs.kitty = {
+      enable = true;
+      font = {
+        name = cfg.font;
+        size = cfg.fontSize;
+      };
+      themeFile = "tokyo_night_night";
+      settings = {
+        enable_audio_bell = false;
+        tab_bar_style = "powerline";
+        confirm_os_window_close = 0;
+      };
+      # Kitty_mod = ctrl+shift
+      keybindings = {
+        "kitty_mod+escape" = ""; # Disable kitty shell
+      };
     };
-    themeFile = "tokyo_night_night";
-    settings = {
-      enable_audio_bell = false;
-      tab_bar_style = "powerline";
-      confirm_os_window_close = 0;
-    };
-    # Kitty_mod = ctrl+shift
-    keybindings = {
-      "kitty_mod+escape" = ""; # Disable kitty shell
+
+    # TODO: Add a proper setting for the default terminal
+    home.sessionVariables = {
+      TERMINAL = "kitty";
     };
   };
 }
