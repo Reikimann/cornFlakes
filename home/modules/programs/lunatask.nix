92377ff5239c007e00475df149a7b3ad3aff834b
Reikimann <cbirkmand@gmail.com>
2025-02-02 21:46:41 +0100

    Added wayland flags to lunatask

--- home/modules/programs/lunatask.nix
+++ home/modules/programs/lunatask.nix
@@ -7,11 +7,27 @@ in
 {
   options.reiki.modules.programs.lunatask = {
     enable = mkEnableOption "Lunatask configuration";
+    enableWayland = mkOption {
+      description = "Toggle to enable Wayland for Lunatask";
+      default = true;
+      type = types.bool;
+    };
   };
 
   config = mkIf cfg.enable {
-    home.packages = with pkgs; [
-      lunatask
+    home.packages = [
+      (if cfg.enableWayland then
+        pkgs.symlinkJoin {
+          name = "lunatask";
+          paths = [ pkgs.lunatask ];
+          buildInputs = [ pkgs.makeWrapper ];
+          postBuild = ''
+          wrapProgram $out/bin/lunatask \
+          --add-flags "--ozone-platform-hint=auto --enable-features=WaylandWindowDecorations --enable-wayland-ime=true"
+          '';
+        }
+      else
+        pkgs.lunatask)
     ];
   };
 }
