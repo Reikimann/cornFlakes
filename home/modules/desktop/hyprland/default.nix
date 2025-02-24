c2eb83dc2df2ea68e35be6bd9dac08c9ae24c4f9
Reikimann <cbirkmand@gmail.com>
2025-02-24 16:46:44 +0100

    Adjusted volume step

--- home/modules/desktop/hyprland/default.nix
+++ home/modules/desktop/hyprland/default.nix
@@ -182,8 +182,8 @@ in
           "$mainMod,dead_diaeresis,exec,${keepassxc}"
           #"$mainMod,e,exec, emacs"
           # Volume
-          ",XF86AudioRaiseVolume,exec,${wpctl} set-volume @DEFAULT_SINK@ 5%+"
-          ",XF86AudioLowerVolume,exec,${wpctl} set-volume @DEFAULT_SINK@ 5%-"
+          ",XF86AudioRaiseVolume,exec,${wpctl} set-volume @DEFAULT_SINK@ 2%+"
+          ",XF86AudioLowerVolume,exec,${wpctl} set-volume @DEFAULT_SINK@ 2%-"
           ",XF86AudioMute,exec,${wpctl} set-mute @DEFAULT_SINK@ toggle"
           ",XF86AudioMicMute,exec,${wpctl} set-mute @DEFAULT_SOURCE@ toggle"
           # Brightness
--- hosts/braize/configuration.nix
+++ hosts/braize/configuration.nix
@@ -47,9 +47,9 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
+  #services.xserver.desktopManager.gnome.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   programs.dconf.enable = true;
-  #services.xserver.desktopManager.gnome.enable = true;
 
   security.polkit.enable = true;
   services.gnome.gnome-keyring.enable = true;
