9fd93ba1e33bbaf0531590a3e0bf01da2ba8e112
Reikimann <cbirkmand@gmail.com>
2025-10-19 15:10:21 +0200

    Added gaming stuff

--- system/common/gaming.nix
+++ system/common/gaming.nix
@@ -1,12 +1,15 @@
-{ ... }:
+{ pkgs, ... }:
+
+# gamemoderun gamescope -f -r 75 -o 25 -- env MANGOHUD=1 %command% 
+# If OpenGL game: gamemoderun gamescope -f -r 75 -o 25 -- mangohud --dlsym %command%
 {
   programs.steam.enable = true;
 
   # TODO: Figure out why and how to use:
-  #programs.steam.gamescopeSession.enable = true;
+  programs.steam.gamescopeSession.enable = true;
 
-  #home.packages = with pkgs; [
-  #  mangohud
-  #];
-  #programs.gamemode.enable = true;
+  environment.systemPackages = with pkgs; [
+    mangohud
+  ];
+  programs.gamemode.enable = true;
 }
